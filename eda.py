1094327c3c1627fa8c02d3d54edff05d0a1fb1a1
geonhwi05 <iwhnoegmik@gmail.com>
2024-04-25 11:38:39 +0900

    version 1.0

--- eda.py
+++ eda.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
 
 def run_eda():
     st.subheader('탐색적 데이터 분석')
@@ -21,4 +23,27 @@ def run_eda():
 
     st.text('컬럼을 선택하면, 각 컬럼별 최대/최소 데이터를 보여드립니다.')
     column_list = ['Age','Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
-    st.selectbox('컬럼을 선택하세요.', column_list)
+    choice_column = st.selectbox('컬럼을 선택하세요.', column_list)
+
+    st.info(f'선택하신 {choice_column} 의 최대 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].max(), ])
+
+    st.info(f'선택하신 {choice_column} 의 최소 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].min(), ])
+
+    st.subheader('상관 관계 분석')
+    st.text('컬럼들을 2개 이상 선택하면 컬럼들의 상관 계수를 보여드립니다.')
+
+    corr_column_list = ['Age','Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
+    selected_columns = st.multiselect('컬럼을 선택하세요.', corr_column_list)
+
+    # 2개 이상 선택했을 때와 그렇지 않을 때로 개발
+    if len(selected_columns) >= 2:
+        # 1. 페어플롯을 그린다.        
+        fig1 = sb.pairplot(data=df, vars=selected_columns)
+        st.pyplot(fig1)
+
+        # 2. 상관계수 보여준다.
+        st.dataframe(df[selected_columns].corr())
+    else:
+        st.text('컬럼을 2개 이상 선택해야 합니다.')
